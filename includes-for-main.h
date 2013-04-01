11e97b766427165fa41c31a0df352bcbd685db5f
Michael Lynch <prophessorsnob@gmail.com>
2013-04-01 15:17:16 +0100

    adding required includes

---
+++ includes-for-main.h
@@ -0,0 +1,15 @@
+//
+//  includes-for-main.h
+//  space-station-build
+//
+//  Created by Michael Lynch on 01/04/2013.
+//  Copyright (c) 2013 Michael Lynch. All rights reserved.
+//
+
+#ifndef space_station_build_includes_for_main_h
+#define space_station_build_includes_for_main_h
+
+#include <GLUT/GLUT.h>
+#include <OpenGL/OpenGL.h>
+
+#endif
--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@
 //
 
 #include <iostream>
+#include "includes-for-main.h"
+using namespace std;
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    cout << "HELLO WORLD" << endl;
 }
 
