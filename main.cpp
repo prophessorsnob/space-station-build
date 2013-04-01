d348e3ef2f9888fb10bf4904bef54044b2f05725
Michael Lynch <prophessorsnob@gmail.com>
2013-04-01 14:28:31 +0100

    Initial source code commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  space-station-build
+//
+//  Created by Michael Lynch on 01/04/2013.
+//  Copyright (c) 2013 Michael Lynch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
