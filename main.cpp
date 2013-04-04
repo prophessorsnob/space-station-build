ff2e5cb2a66d4dbf935e0b4a5d6c57a3158fb5fe
Michael Lynch <prophessorsnob@gmail.com>
2013-04-04 11:14:00 +0100

    All glut window functions are now moved to the window class. This class also has the ability to output the width and height of the scene aswell as the title.

--- main.cpp
+++ main.cpp
@@ -11,18 +11,29 @@
 
 using namespace std;
 
+window glWindow;
+
 int main(int argc, char ** argv)
 {
     cout << "Space Station Build Game" << endl;
     
-    int x = 100;
-    int y = 100;
-    int width = 640;
-    int height = 480;
-    
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-    initGlutWindow(x,y,width,height,"Space Station Build Game",render);
+    
+    glWindow.setPosition(100,100);
+    glWindow.setDimensions(640, 480);
+    glWindow.setTitle("HELLO WORLD");
+    glWindow.setDisplayFunction(render);
+    
+    cout << "Width:" << glWindow.getWidth() << endl;
+    cout << "Height:" << glWindow.getHeight() << endl;
+    
+    cout << "Title:" << glWindow.getTitle() << endl;
+    
+    
+    glWindow.createWindow();
+    
+    //initGlutWindow(x,y,width,height,"Space Station Build Game",render);
 }
 
 
--- window.cpp
+++ window.cpp
@@ -8,6 +8,43 @@
 
 #include "window.h"
 
+
+void window::setPosition(int x, int y){
+    xp = x;
+    yp = y;
+}
+void window::setDimensions(int width, int height){
+    widthp = width;
+    heightp = height;
+}
+void window::setTitle(char *title) {
+    titlep = title;
+}
+
+void window::setDisplayFunction(void (*func)()){
+    function = func;
+}
+
+int window::getWidth(){
+    return widthp;
+}
+int window::getHeight(){
+    return heightp;
+}
+
+char *window::getTitle(){
+    return titlep;
+}
+
+
+void window::createWindow(){
+    glutInitWindowPosition(xp, yp);
+    glutInitWindowSize(widthp, heightp);
+    glutCreateWindow(titlep);
+    glutDisplayFunc(function);
+    glutMainLoop();
+}
+
 void initGlutWindow(int x, int y, int width, int height, const char *title, void (*func)()){
     glutInitWindowPosition(x, y);
     glutInitWindowSize(width, height);
